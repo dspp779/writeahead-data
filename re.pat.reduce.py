2da575f8c190b38b96f4ea072665f97d1aa4be99
JJC <jjc@nlplab.cc>
2015-11-21 12:05:18 +0800

    complete reducer

--- re.pat.reduce.py
+++ re.pat.reduce.py
@@ -53,15 +53,27 @@ class wordPat(Counter):
     
 
 #difficulty:N	difficulty about something	difficulty about learning	difficulty about multiagent learning	1	4	8
-def line_to_headword(x):
-    return x.strip().split('\t')[0].decode('unicode_escape')
+def line_to_word(x):
+    return x.strip().split('\t')[0]
     
 def line_to_pat(x):
-    return x.strip().split('\t')[1].decode('unicode_escape')
+    return x.strip().split('\t')[1]
+    
+def line_to_col(x):
+    return x.strip().split('\t')[2]
+    
+def line_to_ngram(x):
+    return x.strip().split('\t')[3]
+    
+def line_to_rest(x):
+    return '\t'.join(x.strip().split('\t')[2:4])
 
 if __name__ == '__main__':
     table = {}
-    for headword, lines in groupby(fileinput.input(), key=line_to_headword):
+    for word, lines in groupby(fileinput.input(), key=line_to_word):
+        # ISSUE: The input is sorted only by key, direct groupby doesn't perform as expected - same pats are grouped
+        # patInstances = [ (pat, list(instances)) for pat, instances in groupby(lines, key=line_to_pat) ]
+        # FIX: sort by pat first so that instances with the same patterns would be grouped correctly
         lines = sorted(lines)
 
         patInstances = { pat: list(instances) for pat, instances in groupby(lines, key=line_to_pat) }
@@ -73,6 +85,57 @@ if __name__ == '__main__':
 
         goodPats = list(patterns.gen_goodpat())
         
-        for pat, count in goodPats:
-            print ('%s\t%s\t%d' % (headword, pat, count)).encode('utf-8')
+        if goodPats:
+            #print
+            #print '%s (%s)'%(word, sum([ count for _, count in goodPats])) #, goodPats
+            #print
+            table[word] = [ sum( count for _, count in goodPats ) ]
+        else:
+            continue
+            
+        for pat, count in goodPats[:7]:
+            #print '\t*1*%s (%s)'%(pat, count)
+            colInstances = { col: list(instances) for col, instances in groupby(patInstances[pat], key=line_to_col) }
+            colCounts = { col: len(instances) for col, instances in colInstances.iteritems() }
 
+            cols = wordPat()
+            cols.update( colCounts )
+            cols.calc_metrics()
+            goodCols = list(cols.gen_goodpat())
+            #print '\t*2*%s (%s)'%(pat, patCounts[pat]), goodCols
+
+            # TODO: best ngram selection
+            #   Ngram used to be one chunk before pattern, so it's common to see instances with the same ngram
+            #   While the ngram starts from the beginning of the sentence now
+            #   Is it appropriate to select the best ngram by their frequecies? while they might all be 1
+            if not goodCols:
+                #print '\t%s (%s)'%(pat, patCounts[pat]) #, goodCols
+                #print
+                bestngram = max( (len(list(instances)), ngram) for ngram, instances in groupby(patInstances[pat], key=line_to_ngram) )
+                #print '\t\t%s (%s, %s)'%(bestngram[1], colCounts[col], bestngram[0])
+
+                # ISSUE: variable 'col' not exists
+                # FIX: total count
+                # table[word] += [ [pat, patCounts[pat], [(bestngram[1], colCounts[col], bestngram[0])]] ]
+                counts = sum( colCounts.values() )
+                table[word] += [ [pat, patCounts[pat], [(bestngram[1], count, bestngram[0])]] ]
+
+                #print
+                continue
+            res = []
+            for col, count in goodCols[:5]:
+                bestngram = max( (len(list(instances)), ngram) for ngram, instances in groupby(colInstances[col], key=line_to_ngram) )
+                #print '\t\t%s (%s, %s)'%(bestngram[1], colCounts[col], bestngram[0])
+                res += [(bestngram[1], colCounts[col], bestngram[0])]
+            table[word] += [ [pat, patCounts[pat], res] ]
+            #print
+            
+    #print table
+    print json.dumps(table)
+
+'''with open('test.json.txt', 'w') as outfile:
+     json.dump(table, outfile)
+table = {}
+with open('test.json.txt', 'r') as infile:
+    table = json.load(infile)
+print table'''
